e4aab5afba46a287208321a46945edfd81a3fbea
Ludvig <ludvig.dahlgren05@gmail.com>
2025-09-26 17:38:31 +0200

    changes

--- other/practise.hs
+++ other/practise.hs
@@ -118,8 +118,115 @@ add' x y = x + y
 
 
 
---testinggo :: [Int] -> Int
-testinggo xs = go [] re
-    where
-        go [] re = []
-        go (x:xs) re = x + 1 : go xs
+--['a', 'b', 'c'] ::[Char]
+--('a', 'b', 'c') :: (Char,Char,Char)
+--[(Bool, Char)]
+--([Bool], [Char])
+--[([a] -> [a])]
+
+bools :: [Bool]
+bools = [True, False, False]
+
+nums :: [[Int]]
+nums = [[2,3,4],[2,3]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x*y*z
+
+copy :: a -> (a,a)
+copy x = (x, x)
+
+apply :: (a -> b) -> a -> b
+apply f x =  f x
+
+id' :: Num a => a -> a
+id' x = x + 1
+
+id'' :: a -> a
+id'' x = x
+
+const' :: a -> b -> a
+const' x  _ = x
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+snd' :: (a,b) -> b
+snd' (a,b) = b
+
+fst' :: (a,b) -> a
+fst' (a,b) = a
+
+singleton :: a -> [a]
+singleton a = [a]
+
+doubleApply :: (a -> a) -> a -> a
+doubleApply f x = f (f x)
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+applyBoth :: (a -> b) -> (a, a) -> (b, b)
+applyBoth f (x, y) = (f x, f y)
+
+map' :: (a -> b) -> [a] -> [b]
+--map' f xs = [f t | t <- xs]
+
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+maybeApply :: (a -> b) -> Maybe a -> Maybe b
+maybeApply _ Nothing = Nothing
+maybeApply f (Just a) = Just (f a)
+
+eitherLength :: Either [a] [b] -> Int
+eitherLength (Left xs) = length xs
+eitherLength (Right ys) = length ys
+
+listToMaybe' :: [a] -> Maybe a
+listToMaybe' [] = Nothing
+listToMaybe' (x:xs) = Just x 
+
+headOr :: a -> [a] -> a
+headOr x ys = x
+
+choose :: (a -> Bool) -> a -> a -> a
+choose f x y = if f x then x else y
+
+applyList :: [a -> b] -> a -> [b]
+applyList fs x = [f x | f <- fs]
+
+on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+on f g x y = f (g x) (g y)
+
+join :: (a -> a -> a) -> [a] -> a
+join f (x:xs) = f x (join f xs)
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (b, a) -> (a, b)
+swap (x,y) = (y,x)
+
+pair' :: a -> b -> (a, b)
+pair' x y = (x, y)
+
+double''' :: Num x => x -> x
+double''' x = x*2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice'' :: (a -> a) -> a -> a
+twice'' f x = f (f x)
+
+
+
+
+
